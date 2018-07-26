238952e38aa6f47e292da5eaaa621a532402caad
babusid <sidharth.n.babu@gmail.com>
2018-07-26 16:28:42 -0500

    Add files via upload

---
+++ Scripts/secondary.py
@@ -0,0 +1,20 @@
+from interfaces import*
+
+
+class SecondaryInterface:
+
+    def __init__(self, robotname, linear, angular):
+        # default motion initiation
+        self.linear = linear
+        self.angular = angular
+        self.pint = primary_interface(robotname)
+        self.pint.update_body_vel(linear, angular)
+        # sensor interrupt loop start
+
+    def default(self):
+        self.pint.update_body_vel(self.linear, self.angular)
+
+    def interrupt(self):
+        pass
+
+
