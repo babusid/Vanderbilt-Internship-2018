4b6d36397d6f0de2464da10d8becf568182d985d
babusid <sidharth.n.babu@gmail.com>
2018-07-26 16:01:34 -0500

    Add files via upload

--- Scripts/BasicMove.py
+++ Scripts/BasicMove.py
@@ -1,8 +1,22 @@
-from secondary import secondary_interface
-from interfaces import*
+from interfaces import *
 import rospy
-import time
+import math
+
+
+class SecondaryInterface():
+
+    def __init__(self, robotname, linear, angular):
+        # default motion initiation
+        self.linear = linear
+        self.angular = angular
+        self.pint = primary_interface(robotname)
+        self.pint.update_body_vel(linear, angular)
+        # sensor interrupt loop code
+    def DefaultMotion(self):
+        self.pint.update_body_vel(self.linear, self.angular)
+
+
 if __name__ == "__main__":
-    rospy.init_node('drivetest', anonymous=True)
-    miro1 = secondary_interface('rob01')
+    rospy.init_node('DriveTest', anonymous=True)
+    miro1 = SecondaryInterface('rob01', 1, 1)
     rospy.spin()
