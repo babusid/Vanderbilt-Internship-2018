8913d2783f15ef0b95cbb68a9f78043520065c5f
abima <sidharth.n.babu@gmail.com>
2018-07-27 14:12:44 -0500

    Create secondary interface class

---
+++ Scripts/secondary.py
@@ -0,0 +1,15 @@
+from interfaces import*
+import rospy
+
+class secondary_interface
+    def __init__(self, robotname, linear, angular):
+        self.default_linear=linear
+        self.default_angular=angular
+        self.pint = primary_interface(robotname)
+        self.pint.update_body_vel(linear,angular)
+
+    def defaultstate(self):
+        self.pint.update_body_vel(self.default_linear, self.default_angular)
+
+    def sensorinterrupt(self):
+        pass
