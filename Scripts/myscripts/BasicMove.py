a2ff0a18f4527bc02d73c4fab3a5b0336e7975d0
abima <sidharth.n.babu@gmail.com>
2018-07-27 13:52:32 -0500

    filesystem restructured

--- Scripts/myscripts/BasicMove.py
+++
@@ -1,8 +0,0 @@
-import rospy
-from secondary import*
-
-if __name__ == "__main__":
-    rospy.init_node('DriveTest', anonymous=True)
-    miro1 = SecondaryInterface('rob01', 1, 1)
-
-    rospy.spin()
