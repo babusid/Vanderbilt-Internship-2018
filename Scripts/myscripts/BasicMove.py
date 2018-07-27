ae1bba302220df163471677d73ae321e64246b80
sidharth.n.babu@gmail.com <sidharth.n.babu@gmail.com>
2018-07-27 13:49:28 -0500

    Filesystem restructure

--- Scripts/myscripts/BasicMove.py
+++
@@ -1,9 +0,0 @@
-import rospy
-from secondary import*
-
-if __name__ == "__main__":
-    rospy.init_node('DriveTest', anonymous=True)
-    miro1 = SecondaryInterface('rob01', 1, 1)
-    while True:
-        miro1.interrupt()
-    rospy.spin()
--- Scripts/myscripts/Touchtest.py
+++
@@ -1,5 +0,0 @@
-from interfaces import*
-
-tester = primary_interface()
-while True:
-    print(tester.touch_head)
