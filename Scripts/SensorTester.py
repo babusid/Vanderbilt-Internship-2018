0b4687969b090fa7bb5f8ca8b3f36b3620e2437c
sidharth.n.babu@gmail.com <sidharth.n.babu@gmail.com>
2018-07-30 07:43:37 -0500

    current version of sensor tester

--- Scripts/SensorTester.py
+++ Scripts/SensorTester.py
@@ -3,6 +3,5 @@ import rospy
 
 rospy.init_node('tester', anonymous=True)
 tester = primary_interface('rob01')
-while True:
-    print(type(tester.accel_head))
-    print(tester.accel_head)
+print(type(tester.joint_state))
+print(tester.joint_state)
