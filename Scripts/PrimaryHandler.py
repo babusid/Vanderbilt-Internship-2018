d2ec99951d494a55ed97f99a99f3314984806a09
abima <sidharth.n.babu@gmail.com>
2018-07-30 16:22:55 -0500

    Got rid of pet_test conglomeration, because the algorithm was inaccurate. Reverted to simple head/body touch loop.

--- Scripts/PrimaryHandler.py
+++ Scripts/PrimaryHandler.py
@@ -21,93 +21,30 @@ class SecondaryInterface:
     def __init__(self, robotname, linear, angular):
         self.default_linear = linear
         self.default_angular = angular
-        self.pint = primary_interface(robotname)
+        self.pint =primary_interface(robotname)
         self.pint.update_body_vel(linear, angular)
 
     def defaultmovestate(self):
         self.pint.update_body_vel(self.default_linear, self.default_angular)
 
-    """def sensorinterrupt(self):
-        if 1 in self.pint.touch_body:
-            self.pint.stop_moving()
-            self.pint.tail_move()
-        elif 1 in self.pint.touch_head:
-            self.pint.stop_moving()
-            self.pint.tail_move()
-        else:
-
-            self.defaultmovestate()
-"""
-    def data_in(self):
-
-        start_time = rospy.get_rostime()
-        self.old_value = 0.0
-        flag_pet = False
-        flag_pat = False
-        self.pint.body_config_speed = [5, 5, 5, 5]
-
+    def sensorinterrupt(self):
         while not rospy.is_shutdown():
 
-            if self.pint.touch_body:
+            if 1 in self.pint.touch_body:
+                self.pint.stop_moving()
+                self.pint.tail_move()
+                self.pint.body_config = [0, 0, 0, 1]
+                time.sleep(.5)
+                self.pint.body_config = [0, 0, 0, 0]
 
-                # find average value of body touch
-                try:
-                    self.value = (self.pint.touch_body[0] + 2.0 * self.pint.touch_body[1] + 3.0 *
-                                  self.pint.touch_body[2] + 4.0 * self.pint.touch_body[3]) / (
-                                     numpy.sum(self.pint.touch_body))
-                except ZeroDivisionError:
-                    self.value = 0
-
-                # test for 2 different types of touching (petting and patting)
-                if abs(self.value - self.old_value) > .5:
-
-                    # No flags activated
-                    if flag_pet == False and flag_pat == False:
-
-                        if self.value != 0:
-                            flag_pet = True
-                            flag_pat = False
-                        else:
-                            flag_pet = False
-                            flag_pat = True
-                        print flag_pet, flag_pat
-                        start_time = rospy.get_rostime()
-                    # one flag activated
-                    elif flag_pet == True:
-
-                        # if next event occurs within 1 sec, initiates response
-                        if self.value != 0 and (rospy.get_rostime() - start_time).to_sec() < 1.0:
-                            self.pet_detect()
-                            flag_pet = False
-                            time.sleep(.5)
-                        else:
-                            flag_pet = False
-                    elif flag_pat == True:
-
-                        if (rospy.get_rostime() - start_time).to_sec() < 1.0:
-                            self.pat_detect()
-                            flag_pat = False
-                            time.sleep(.5)
-                        else:
-                            flag_pat = False
-                self.old_value = self.value
+            elif 1 in self.pint.touch_head:
+                self.pint.stop_moving()
+                self.pint.tail_move()
+                # nod head in approval
+                self.pint.body_config = [0, 0, 0, 1]
+                time.sleep(.5)
+                self.pint.body_config = [0, 0, 0, 0]
             else:
+
                 self.defaultmovestate()
 
-    def pet_detect(self):
-        # nod head in approval and wag tail
-        self.pint.stop_moving()
-        self.pint.tail_move()
-        self.pint.body_config = [0, 0, 0, 1]
-        time.sleep(.5)
-        self.pint.body_config = [0, 0, 0, 0]
-
-    def pat_detect(self):
-        # shake head in disapproval and droop tail
-        self.pint.stop_moving()
-        self.pint.tail_move(-1)
-        self.pint.body_config = [0, 0, .2, 0]
-        time.sleep(.25)
-        self.pint.body_config = [0, 0, -2, 0]
-        time.sleep(.25)
-        self.pint.body_config = [0, 0, 0, 0]
